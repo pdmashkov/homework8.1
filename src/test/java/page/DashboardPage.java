5b7c981e2ef7e846bf6430f0f3ea2ef6dc5de960
pdmashkov <pdmashkov@rolftech.ru>
2025-02-02 19:08:46 +0300

    add DashboardPage

---
+++ src/test/java/page/DashboardPage.java
@@ -0,0 +1,14 @@
+package page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class DashboardPage {
+    private SelenideElement header = $("[data-test-id='dashboard']");
+
+    public DashboardPage() {
+        header.shouldBe(Condition.visible);
+    }
+}
